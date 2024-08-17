fad62b57c26bd0a42097cbce9d85cfef42d04b20
Ancy <bsapanacode@gmail.com>
2024-08-18 00:56:54 +0530

    new update on menurepo

--- NewProgram1/src/com/ust/HelloWorld.java
+++ NewProgram1/src/com/ust/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("New message");
+		System.out.println("New Update");
 	}
 
 }
